bfa9904e79865416a44dddda568d0a59f127caeb
Sebastian Bernauer <sebastian.bernauer@1und1.de>
2020-02-27 10:31:53 +0100

    Initial import

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Build linux BPF samples
+git clone --depth 10 https://github.com/torvalds/linux
+
+cd linux
+make localmodconfig
+make prepare
+make headers_install 
+
+cd samples/bpf
+make
+
+# Copy pingxelflut to linux kernel source
+cd ../../..
+cp xdp_pingxelflut_kern.c xdp_pingxelflut_user.c linux/samples/bpf
+
+# Add new pingxelflut to Makefile
+cd linux/samples/bpf
+sed -i -e 's/tprogs-y += xdp1/tprogs-y += xdp_pingxelflut\ntprogs-y += xdp1/' Makefile
+sed -i -e 's/xdp1-objs := xdp1_user.o/xdp_pingxelflut-objs := xdp_pingxelflut_user.o\nxdp1-objs := xdp1_user.o/' Makefile
+sed -i -e 's/always-y += xdp1_kern.o/always-y += xdp_pingxelflut_kern.o\nalways-y += xdp1_kern.o/' Makefile
+make
