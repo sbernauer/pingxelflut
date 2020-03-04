fd127cba4368eede2db4cad5475349f530853c24
Sebastian Bernauer <sebastian.bernauer@1und1.de>
2020-03-04 15:32:41 +0100

    Added xdp_counter as copy of xdp1

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@
 
 # Build pingxelflut
 ## Build linux BPF samples
-git clone --depth 10 https://github.com/torvalds/linux
+if [[ -d linux ]]; then
+	echo "Skipped cloning of linux kernel source"
+else
+	git clone --depth 10 https://github.com/torvalds/linux
+fi
 
 num_cpus=$(nproc --all)
 
@@ -19,12 +23,18 @@ make
 ## Copy pingxelflut to linux kernel source
 cd ../../..
 cp xdp_pingxelflut_kern.c xdp_pingxelflut_user.c linux/samples/bpf
-
-## Add new pingxelflut to Makefile
 cd linux/samples/bpf
 sed -i -e 's/tprogs-y += xdp1/tprogs-y += xdp_pingxelflut\ntprogs-y += xdp1/' Makefile
 sed -i -e 's/xdp1-objs := xdp1_user.o/xdp_pingxelflut-objs := xdp_pingxelflut_user.o\nxdp1-objs := xdp1_user.o/' Makefile
 sed -i -e 's/always-y += xdp1_kern.o/always-y += xdp_pingxelflut_kern.o\nalways-y += xdp1_kern.o/' Makefile
+
+## Copy counter to linux kernel source
+cd ../../..
+cp xdp_counter_kern.c xdp_counter_user.c linux/samples/bpf
+cd linux/samples/bpf
+sed -i -e 's/tprogs-y += xdp1/tprogs-y += xdp_counter\ntprogs-y += xdp1/' Makefile
+sed -i -e 's/xdp1-objs := xdp1_user.o/xdp_counter-objs := xdp_counter_user.o\nxdp1-objs := xdp1_user.o/' Makefile
+sed -i -e 's/always-y += xdp1_kern.o/always-y += xdp_counter_kern.o\nalways-y += xdp1_kern.o/' Makefile
 
 ## Build pingxelflut kernel and userspace programs
 make -j $num_cpus
